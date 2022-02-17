003e5c030e5cac7c686f4c939bbf793a23405249
GenerallyIntelligent <tannerjeffreysims@gmail.com>
2022-02-17 08:06:57 -0700

    Implement auto multitask detection

--- hatecomp/models/auto.py
+++ hatecomp/models/auto.py
@@ -1,6 +1,6 @@
 # Use the implementation of https://github.com/shahrukhx01/multitask-learning-transformers/tree/main/multiple_prediction_heads to allow for multitask
 # Figure out how to update the datasets which have multiple tasks for this
-
+import torch
 from transformers import AutoModelForSequenceClassification
 
 
@@ -9,9 +9,14 @@ class HatecompAutoModelForSequenceClassification(AutoModelForSequenceClassificat
         model = AutoModelForSequenceClassification.from_pretrained(
             transformer_name, num_labels=num_labels
         )
-        if len(num_labels) > 1:
-            model.config.multiheaded = True
-        else:
+        if isinstance(num_labels, int):
             model.config.multiheaded = False
+        elif isinstance(num_labels, (tuple, list)):
+            if len(num_labels) > 1:
+                model.config.multiheaded = True
+            else:
+                model.config.multiheaded = False
+        elif isinstance(num_labels, torch.Tensor):
+            
 
         print(model.classification_head)
--- setup.py
+++ setup.py
@@ -2,26 +2,26 @@ import os
 from setuptools import setup, find_packages
 
 PACKAGE_ROOT = os.path.dirname(os.path.realpath(__file__))
-README_FILE = open(os.path.join(PACKAGE_ROOT, 'README.md'), 'r').read()
+README_FILE = open(os.path.join(PACKAGE_ROOT, "README.md"), "r").read()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(
-        name = 'hatecomp',
-        version = '0.2.0',
-        description = "Collection of pytorch datasets for hate speech and toxic internet discourse",
-        long_description = README_FILE,
-        long_description_content_type = "text/markdown",
-        url = 'https://github.com/GenerallyIntelligent/hatecomp',
-        author = 'GenerallyIntelligent',
-        author_email = 'tannersims@generallyintelligent.me',
-        license = 'MIT',
-        packages = find_packages(),
-        install_requires = [
-            'aiometer >=0.3.0, <1.0.0',
-            'httpx >=0.21.1, <1.0.0',
-            'requests >=2.0.0, <3.0.0',
-            'torch >=1.7.0, <2.0.0',
-            'tqdm >=4.1.0, <5.0.0',
-            'tweepy >=4.0.0, <5.0.0'
-        ]
+        name="hatecomp",
+        version="0.2.0",
+        description="Collection of pytorch datasets for hate speech and toxic internet discourse",
+        long_description=README_FILE,
+        long_description_content_type="text/markdown",
+        url="https://github.com/GenerallyIntelligent/hatecomp",
+        author="GenerallyIntelligent",
+        author_email="tannersims@generallyintelligent.me",
+        license="MIT",
+        packages=find_packages(),
+        install_requires=[
+            "aiometer >=0.3.0, <1.0.0",
+            "httpx >=0.21.1, <1.0.0",
+            "requests >=2.0.0, <3.0.0",
+            "torch >=1.7.0, <2.0.0",
+            "tqdm >=4.1.0, <5.0.0",
+            "tweepy >=4.0.0, <5.0.0",
+        ],
     )
