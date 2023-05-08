268044604477725ef7d7bad317020af836770df6
GenerallyIntelligent <tannerjeffreysims@gmail.com>
2023-05-08 13:41:32 -0600

    Fix version and bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README_FILE = open(os.path.join(PACKAGE_ROOT, "README.md"), "r").read()
 if __name__ == "__main__":
     setup(
         name="hatecomp",
-        version="0.2.0",
+        version="0.4.0",
         description="Collection of pytorch datasets for hate speech and toxic internet discourse",
         long_description=README_FILE,
         long_description_content_type="text/markdown",
